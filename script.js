a8d05f79e28156e90ef987e8ebe0ac4bb59538ef
Ondra Svec <ondrasvecveslo@gmail.com>
2023-03-09 09:58:06 +0100

    Fix user interaction animations
    Previously, the focus-in/out animations on required fields worked only
    during the first iteration. Once the label turned back red on focusout,
    they didn't get the purple color on focusin.
    
    Now, the app handles user input, focusin and focusout scenarios by way
    of if statements and applies or removes the relevant styles/animations.

--- script.js
+++ script.js
@@ -16,45 +16,78 @@ const confirm_psw_LCN = document.querySelector('#confirm_psw-LCN');
 
 inputs.forEach(input => input.addEventListener('input', handleUserInput));
 
+inputs.forEach(input => input.addEventListener('focusin', handleFocusIn));
+
 inputs.forEach(input => input.addEventListener('focusout', handleFocusOut));
 
 function handleUserInput(e) {
     switch (e.target.id) {
         case 'fname':
-            if (e.target.value !== "" && e.target.checkValidity() === true) {
+            if ((e.target.value !== "" || e.target.value !== null) && e.target.checkValidity() === true) {
                fname_LCN.classList.add('active');
             }
         break;
         case 'lname':
-            if (e.target.value !== "" && e.target.checkValidity() === true) {
+            if ((e.target.value !== "" || e.target.value !== null) && e.target.checkValidity() === true) {
                lname_LCN.classList.add('active');
             }
         break;
         case 'phone':
-            if (e.target.value !== "" && e.target.checkValidity() === true) {
+            if ((e.target.value !== "" || e.target.value !== null) && e.target.checkValidity() === true) {
                phone_LCN.classList.add('active');
             }
         break;
         case 'email':
-            if (e.target.value !== "" && e.target.checkValidity() === true) {
+            if ((e.target.value !== "" || e.target.value !== null) && e.target.checkValidity() === true) {
                email_LCN.classList.add('active');
-            } else if (e.target.value !== "" && e.target.checkValidity() === false) {
+            } else if ((e.target.value !== "" || e.target.value !== null) && e.target.checkValidity() === false) {
+                email_LCN.classList.remove('active');
                 email_LCN.classList.add('active-invalid');
             }
         break;
         case 'psw':
-            if (e.target.value !== "" && e.target.checkValidity() === true) {
+            if ((e.target.value !== "" || e.target.value !== null) && e.target.checkValidity() === true) {
                psw_LCN.classList.add('active');
-            } else if (e.target.value !== "" && e.target.checkValidity() === false) {
+            } else if ((e.target.value !== "" || e.target.value !== null) && e.target.checkValidity() === false) {
+                psw_LCN.classList.remove('active');
                 psw_LCN.classList.add('active-invalid');
             }
         break;
         case 'confirm_psw':
-            if (e.target.value !== "" && e.target.checkValidity() === true) {
+            if ((e.target.value !== "" || e.target.value !== null) && e.target.checkValidity() === true) {
                confirm_psw_LCN.classList.add('active');
-            } else if (e.target.value !== "" && e.target.checkValidity() === false) {
+            } else if ((e.target.value !== "" || e.target.value !== null) && e.target.checkValidity() === false) {
+                confirm_psw_LCN.classList.remove('active');
                 confirm_psw_LCN.classList.add('active-invalid');
             }
+        break;
+        default:
+            return;
+    }
+}
+
+function handleFocusIn(e) {
+    switch (e.target.id) {
+        case 'fname':
+                fname_LCN.classList.add('active');
+        break;
+        case 'lname':
+                lname_LCN.classList.add('active');
+        break;
+        case 'phone':
+                phone_LCN.classList.add('active');
+        break;
+        case 'email':
+                email_LCN.classList.add('active');
+                email_LCN.setAttribute('style', 'color: var(--input-purple);')
+        break;
+        case 'psw':
+                psw_LCN.classList.add('active');
+                psw_LCN.setAttribute('style', 'color: var(--input-purple);')
+        break;
+        case 'confirm_psw':
+                confirm_psw_LCN.classList.add('active');
+                confirm_psw_LCN.setAttribute('style', 'color: var(--input-purple);')
         break;
         default:
             return;
@@ -79,24 +112,39 @@ function handleFocusOut(e) {
             }
         break;
         case 'email':
-            if ((e.target.value !== "" || e.target.value === null) && e.target.checkValidity() === true) {
+            if ((e.target.value === '' || e.target.value === null) && e.target.checkValidity() === false) {
+            email_LCN.classList.remove('active-invalid', 'active');
+            email_LCN.setAttribute('style', 'color: red;');
+            } else if (e.target.value !== "" || e.target.value !== null) {
+                if (e.target.checkValidity() === true) {
                 email_LCN.setAttribute('style', 'color: var(--input-purple);');
-            } else if (e.target.value === '' || e.target.value === null) {
-                email_LCN.classList.remove('active-invalid');
+                } else if (e.target.checkValidity() === false) {
+                email_LCN.setAttribute('style', 'color: red;');
+                }
             }
         break;
         case 'psw':
-            if ((e.target.value !== "" || e.target.value === null) && e.target.checkValidity() === true) {
+            if ((e.target.value === '' || e.target.value === null) && e.target.checkValidity() === false) {
+            psw_LCN.classList.remove('active-invalid', 'active');
+            psw_LCN.setAttribute('style', 'color: red;');
+            } else if (e.target.value !== "" || e.target.value !== null) {
+                if (e.target.checkValidity() === true) {
                 psw_LCN.setAttribute('style', 'color: var(--input-purple);');
-            } else if (e.target.value === '' || e.target.value === null) {
-                psw_LCN.classList.remove('active-invalid');
+                } else if (e.target.checkValidity() === false) {
+                psw_LCN.setAttribute('style', 'color: red;');
+                }
             }
         break;
         case 'confirm_psw':
-            if ((e.target.value !== "" || e.target.value === null) && e.target.checkValidity() === true) {
+            if ((e.target.value === '' || e.target.value === null) && e.target.checkValidity() === false) {
+            confirm_psw_LCN.classList.remove('active-invalid', 'active');
+            confirm_psw_LCN.setAttribute('style', 'color: red;');
+            } else if (e.target.value !== "" || e.target.value !== null) {
+                if (e.target.checkValidity() === true) {
                 confirm_psw_LCN.setAttribute('style', 'color: var(--input-purple);');
-            } else if (e.target.value === '' || e.target.value === null) {
-                confirm_psw_LCN.classList.remove('active-invalid');
+                } else if (e.target.checkValidity() === false) {
+                confirm_psw_LCN.setAttribute('style', 'color: red;');
+                }
             }
         break;
         default:
